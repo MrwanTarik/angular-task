5b810755b7624f83a90d899ae7289ff396f80617
MrwanTarik <mrwantarik50@gmail.com>
2024-07-17 02:59:45 +0300

    task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   showBranchDetails: boolean = true;
   showCommerceDetails: boolean = true;
+  branchDetails: Array<any> = [
+    {
+      id: Math.random() * 10000,
+    },
+  ];
   onCheckDepartmentOne(event: Event) {
     const checkbox = event.target as HTMLInputElement;
 
@@ -30,5 +35,17 @@ export class AppComponent {
     } else {
       this.showCommerceDetails = true;
     }
+  }
+  addNewRow(event: Event) {
+    event.preventDefault();
+    this.branchDetails.push({
+      id: Math.random() * 10000,
+    });
+  }
+  deleteRow(event: Event, id: number) {
+    event.preventDefault();
+    this.branchDetails = this.branchDetails.filter((branch) => {
+      return branch.id != id;
+    });
   }
 }
