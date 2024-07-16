819f674ac9dc5911630fc0aabf93831b92ae57f9
MrwanTarik <mrwantarik50@gmail.com>
2024-07-16 15:43:29 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-task';
+  showBranchDetails: boolean = true;
+  showCommerceDetails: boolean = true;
+  onCheckDepartmentOne(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+
+    if (checkbox.checked) {
+      this.showBranchDetails = false;
+    } else {
+      this.showBranchDetails = true;
+    }
+  }
+  onCheckDepartmentTwo(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    if (checkbox.checked) {
+      this.showCommerceDetails = false;
+    } else {
+      this.showCommerceDetails = true;
+    }
+  }
 }
---
+++ src/app/branch-details/branch-details.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-branch-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './branch-details.component.html',
+  styleUrl: './branch-details.component.css',
+})
+export class BranchDetailsComponent {}
